df03f29751875a421deacbea40debd7d467c877d
JOMAALFRED <jomaalfredm@gmail.com>
2026-06-15 21:12:33 +0300

    fix trial 30 jours !

--- dental-pm-mvp/migrations/20260512-clinic-subscription-status-pending.js
+++ dental-pm-mvp/migrations/20260512-clinic-subscription-status-pending.js
@@ -2,11 +2,15 @@
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.sequelize.query(
-      `ALTER TYPE "enum_clinics_subscription_status" ADD VALUE IF NOT EXISTS 'PENDING'`
-    ).catch((error) => {
-      if (!/does not exist|undefined_object/i.test(error.message)) throw error;
-    });
+    const values = ['PENDING', 'TRIAL_EXPIRED', 'CANCELLED', 'SUSPENDED'];
+
+    for (const value of values) {
+      await queryInterface.sequelize.query(
+        `ALTER TYPE "enum_clinics_subscription_status" ADD VALUE IF NOT EXISTS '${value}'`
+      ).catch((error) => {
+        if (!/does not exist|undefined_object/i.test(error.message)) throw error;
+      });
+    }
   },
 
   async down() {
